bce22fe27cd0e8b1fc9b6b93a88738c53b7d9973
Davaabayar <busdavaabayar@gmail.com>
2019-12-12 16:00:53 -0600

    User has a Profile

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into uptake.country values(1,"USA");
+insert into uptake.country values(2,"Mongolia");
+insert into uptake.address values(1, "Fairfield", "Iowa", "52556", 1);
+insert into uptake.user values(1,"2019-12-12","user@uptake.come","Full name","123456", 1, 1);
+insert into uptake.profile values(1,"Bio", "2000-10-10","Dolly", 1, 1, "anna");
