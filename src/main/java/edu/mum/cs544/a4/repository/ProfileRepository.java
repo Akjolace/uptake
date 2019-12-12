e00c2cd103d03bf8f391338476fe828813b6fca7
Davaabayar <busdavaabayar@gmail.com>
2019-12-12 15:03:39 -0600

    User has a Profile

---
+++ src/main/java/edu/mum/cs544/a4/repository/ProfileRepository.java
@@ -0,0 +1,10 @@
+package edu.mum.cs544.a4.repository;
+
+import edu.mum.cs544.a4.entity.Profile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProfileRepository extends JpaRepository<Profile,Long> {
+    Profile findByProfileName(String profileName);
+}
