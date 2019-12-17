4946f65e65329a32a00db128e2a63b619698917f
Akjol <Akjol.igm@gmail.com>
2019-12-17 16:36:00 -0600

    Admin Controller panel

--- src/main/java/edu/mum/cs544/a4/controller/AdminController.java
+++ src/main/java/edu/mum/cs544/a4/controller/AdminController.java
@@ -1,33 +1,65 @@
 package edu.mum.cs544.a4.controller;
 
+import edu.mum.cs544.a4.entity.Ads;
+import edu.mum.cs544.a4.entity.Post;
 import edu.mum.cs544.a4.entity.User;
+import edu.mum.cs544.a4.service.AdsService;
+import edu.mum.cs544.a4.service.PostService;
 import edu.mum.cs544.a4.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class AdminController {
 
     private UserService userService;
+    private PostService postService;
+    private AdsService adsService;
 
-    public AdminController(UserService userService) {
+
+    @Autowired
+    public AdminController(UserService userService, PostService postService, AdsService adsService) {
         this.userService = userService;
+        this.postService = postService;
+        this.adsService = adsService;
+
     }
 
-    @GetMapping({"/admin", "/home"})
+    @GetMapping(value = "/admin")
     public String getAdmin() {
-        return "admin/base";
+        return "admin/admin-base";
     }
 
-    @GetMapping("/users")
-    public List<User> getUserList() {
-        return userService.getAllUser();
+    @GetMapping(value = "/admin/users")
+    public String listUser(@ModelAttribute("users") User user, Model model) {
+        model.addAttribute("users", userService.getAllUser());
+        return "admin/admin-users";
     }
 
-    @GetMapping("/users/{userId}")
+    @PostMapping(value = "/admin/users")
+    public String addUser(@ModelAttribute("users") User user, Model model, RedirectAttributes redirectAttributes) {
+//        model.addAttribute("users", userService.addUser(user));
+        redirectAttributes.addFlashAttribute("users", userService.addUser(user));
+        return "redirect:/admin/users";
+//        return "admin/admin-users";
+    }
+
+    @GetMapping(value = "/admin/posts")
+    public String listPost(@ModelAttribute("posts") Post post, Model model) {
+        model.addAttribute("posts", postService.getAllPost());
+        return "admin/admin-posts";
+    }
+
+    @GetMapping(value = "/admin/ads")
+    public String listAds(@ModelAttribute("ads") Ads ads, Model model) {
+        model.addAttribute("ads", adsService.getAllAds());
+        return "admin/admin-ads";
+    }
+
+    @GetMapping("/admin/users/{userId}")
     public User getUser(@PathVariable Long userId) {
         return userService.getUserById(userId);
         //If employee is not found -> throw Exception
--- src/main/java/edu/mum/cs544/a4/service/PostService.java
+++ src/main/java/edu/mum/cs544/a4/service/PostService.java
@@ -3,6 +3,7 @@ package edu.mum.cs544.a4.service;
 import edu.mum.cs544.a4.entity.Post;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PostService {
@@ -12,4 +13,6 @@ public interface PostService {
     Long editPost(Post post);
 
     Post findPostById(long id);
+
+    public List<Post> getAllPost();
 }
