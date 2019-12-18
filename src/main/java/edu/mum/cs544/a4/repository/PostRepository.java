ecdf5764a4edee17de11aa7687a743761d8a95c2
Issac <onokokono@gmail.com>
2019-12-18 15:11:15 -0600

    needed to pull something

--- src/main/java/edu/mum/cs544/a4/repository/PostRepository.java
+++ src/main/java/edu/mum/cs544/a4/repository/PostRepository.java
@@ -1,12 +1,19 @@
 package edu.mum.cs544.a4.repository;
 
 import edu.mum.cs544.a4.entity.Post;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
 
     Post findById(long id);
+    
+    @Query(value = "", nativeQuery = true)
+    List<Post> findByDescription(String email, String description);
 
 }
