cef794ac8d3a2b6a62d354e4d87abdc9968dd80d
Akjol <Akjol.igm@gmail.com>
2019-12-18 18:53:44 -0600

    Admin User Post dashboard

--- src/main/java/edu/mum/cs544/a4/controller/AdminController.java
+++ src/main/java/edu/mum/cs544/a4/controller/AdminController.java
@@ -4,20 +4,15 @@ import edu.mum.cs544.a4.entity.Ads;
 import edu.mum.cs544.a4.entity.Post;
 import edu.mum.cs544.a4.entity.User;
 import edu.mum.cs544.a4.service.AdsService;
+import edu.mum.cs544.a4.service.CommentService;
 import edu.mum.cs544.a4.service.PostService;
 import edu.mum.cs544.a4.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -26,19 +21,24 @@ public class AdminController {
     private UserService userService;
     private PostService postService;
     private AdsService adsService;
+    private CommentService commentService;
 
     @Autowired
-    public AdminController(UserService userService, PostService postService, AdsService adsService) {
+    public AdminController(UserService userService, PostService postService, AdsService adsService, CommentService commentService) {
         this.userService = userService;
         this.postService = postService;
         this.adsService = adsService;
+        this.commentService = commentService;
 
     }
+    /*-------------------------------BASE--------------------------------- */
 
     @GetMapping(value = "/admin")
     public String getAdmin() {
         return "admin/adminBase";
     }
+
+    /*-------------------------------USER--------------------------------- */
 
     @GetMapping(value = "/admin/users")
     public String listUser(@ModelAttribute("users") User user, Model model) {
@@ -46,11 +46,27 @@ public class AdminController {
         return "admin/adminUsers";
     }
 
-//    @PostMapping(value = "/admin/users")
-//    public String addUser(@ModelAttribute("users") User user, Model model, RedirectAttributes redirectAttributes) {
-//        redirectAttributes.addFlashAttribute("users", userService.addUser(user));
-//        return "redirect:/admin/users";
-//    }
+    @PostMapping("/admin/users")
+    @ResponseBody
+    public String updateUserStatus(@RequestParam("userId") String userId) {
+        User user = userService.getUserById(Long.parseLong(userId));
+        if(user.isStatus())
+            user.setStatus(false);
+        else
+            user.setStatus(true);
+        userService.addUser(user);
+        return  (user.isStatus()) ? "Active" : "Inactive";
+    }
+
+    @GetMapping(path = "/admin/users/{id}")
+    public String viewUser(@PathVariable("id") Long id, Model model) {
+        User user = userService.getUserById(id);
+        model.addAttribute("user", user);
+        model.addAttribute("comments",commentService.getAllByUser(user));
+        return "admin/viewUser";
+    }
+
+    /*-------------------------------POST--------------------------------- */
 
     @GetMapping(value = "/admin/posts")
     public String listPost(@ModelAttribute("posts") Post post, Model model) {
@@ -58,19 +74,6 @@ public class AdminController {
         System.out.println("POST PRINTING ******" + postService.getAllPost());
         return "admin/adminPosts";
     }
-
-    @GetMapping("/admin/users/{userId}")
-    public User getUser(@PathVariable Long userId) {
-        return userService.getUserById(userId);
-        //If employee is not found -> throw Exception
-    }
-
-    @PostMapping("/users")
-    public User addUser(@RequestBody User user) {
-        userService.addUser(user);
-        return user;
-    }
-
 
     /*-------------------------------ADVERTISEMENT--------------------------------- */
     @GetMapping(value = "/admin/ads")
--- src/main/java/edu/mum/cs544/a4/service/CommentService.java
+++ src/main/java/edu/mum/cs544/a4/service/CommentService.java
@@ -4,6 +4,9 @@ import edu.mum.cs544.a4.entity.Comment;
 import edu.mum.cs544.a4.entity.Post;
 import edu.mum.cs544.a4.entity.User;
 
+import java.util.List;
+
 public interface CommentService {
     Comment add(User user, Post post, String description);
+    List<Comment> getAllByUser(User user);
 }
